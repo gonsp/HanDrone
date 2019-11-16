268a144d466490bad00c993a248c3235aa4f456d
David Alvarez <davidalro@gmail.com>
2019-11-17 00:30:46 +0100

    MX Master Controller

--- hand_tracker/motion_capturer.py
+++ hand_tracker/motion_capturer.py
@@ -5,4 +5,7 @@ class MotionCapturer():
     @abstractmethod
     def get_axis_values(self):
         pass
-        
+
+    @abstractmethod
+    def shutdown(self):
+        pass
--- hand_tracker/motion_capturer_mouse.py
+++ hand_tracker/motion_capturer_mouse.py
@@ -1,9 +1,97 @@
 from motion_capturer import MotionCapturer
 
-class MotionCapturerMouse(MotionCapturer):
+import logitech_controller.logidevmon as logidevmon
+import json
+from threading import Thread
 
+def registerEvent(event, capturer):
+    capturer.processEvents(event)
+    return True
+
+def run_loop(capturer):
+    logidevmon.read_events(registerEvent, capturer)
+
+class MotionCapturerMouse(MotionCapturer):
     def __init__(self):
-        pass
+        self.currentThrottle = 0
+        self.currentX = 50
+        self.currentY = 50
+        self.mouseId = 0
+        self.captureThread = None
+        self.noShutdown = True
+        self.captureMouse()
+        
+    def captureMouse(self):
+        logidevmon.list_devices()
+        for device in logidevmon.LOGITECH_DEVICES:
+            print (f"{device['unitId']} {device['type']} : {device['name']}")
+                
+            if (device["type"] == "mouse"):
+                self.mouseId = device['unitId']
+        
+        logidevmon.set_thumbWheel_config(self.mouseId,False,False,False)
+        logidevmon.set_wheel_config(self.mouseId,False,False,False)
+
+        logidevmon.set_thumbWheel_config(self.mouseId,False,False,False)
+        logidevmon.set_wheel_config(self.mouseId,False,False,False)
+
+        if (len(logidevmon.LOGITECH_DEVICES)>0 and self.mouseId != 0):    
+            logidevmon.set_wheel_config(self.mouseId,True,True,True)    
+            logidevmon.set_immediate_pointer_redirection_config(self.mouseId, True, True, True)
+
+            self.captureThread = Thread(target=run_loop, args=(self, ))
+            self.captureThread.start()
 
     def get_axis_values(self):
-        pass
+        res = (float(self.currentY) / 100, float(self.currentX) / 100, 0.5, float(self.currentThrottle) / 100)
+        distance_to_middle = 50 - self.currentY
+        step = float(distance_to_middle) / 50
+        self.currentY = self.currentY + step
+        distance_to_middle = 50 - self.currentX
+        step = float(distance_to_middle) / 50
+        self.currentX = self.currentX + step
+        return res
+
+    def shutdown(self):
+        logidevmon.set_immediate_pointer_redirection_config(self.mouseId, False, False, False)
+        logidevmon.set_wheel_config(self.mouseId,False,False,False)
+        self.noShutdown = False
+
+    def normalize(self, n):
+        if n > 100:
+            n = 100
+        elif n < 0:
+            n = 0
+        return n
+
+    def processWheel(self, event):
+        delta = int(event["value"]["delta"])
+        if delta < 0:
+            delta = delta * 0.4
+        else:
+            delta = delta * 0.6
+        self.currentThrottle = self.normalize(self.currentThrottle + delta)
+
+    def interpretMouse(self, value, mult):
+        if(value == 0):
+            return 0
+        elif(value > 1000):
+            return 3 * mult
+        else:
+            return -3 * mult
+    
+
+    def processXYRaw(self, event):
+        self.currentX = self.normalize(self.currentX + self.interpretMouse(event["value"]["dx"], -1))
+        self.currentY = self.normalize(self.currentY + self.interpretMouse(event["value"]["dy"], 1))
+
+    def processEvents(self, message):
+        res = json.loads(message)
+        print(res)
+
+        if (res["path"] == "wheel"):
+            self.processWheel(res)
+        elif (res["path"] == "divertedRawXY"):
+            self.processXYRaw(res)
+
+        return self.noShutdown
